ed759d9596f81dbf27253202ce24b7eca7929851
Vadim Smal <vvsmal@avito.ru>
2019-02-27 15:59:42 +0300

    MA-1825: Renamed ChecksumObject to Checksummable

--- Sources/ProjectChecksumCalculator/ProjChecksum.swift
+++ Sources/ProjectChecksumCalculator/ProjChecksum.swift
@@ -1,38 +1,38 @@
 import Foundation
 
-protocol ChecksumObject: Hashable {
+protocol Checksummable: Hashable {
     var checksum: String { get }
 }
 
-struct FileChecksum: ChecksumObject {
+struct FileChecksum: Checksummable {
     
     let filePath: String
     let checksum: String
     
 }
 
-struct TargetChecksum: ChecksumObject {
+struct TargetChecksum: Checksummable {
     
     let files: [FileChecksum]
     let checksum: String
     
 }
 
-struct ProjectChecksum: ChecksumObject {
+struct ProjectChecksum: Checksummable {
     
     let targets: [TargetChecksum]
     let checksum: String
     
 }
 
-struct ProjChecksum: ChecksumObject {
+struct ProjChecksum: Checksummable {
     
     let projects: [ProjectChecksum]
     let checksum: String
     
 }
 
-extension Array where Element: ChecksumObject {
+extension Array where Element: Checksummable {
     
     func checksum() throws -> String {
         guard let checksum = try map({ $0.checksum }).joined().md5() else {
