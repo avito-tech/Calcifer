20da2d971c0377a1e326ab4947988a5d2cce6022
Vadim Smal <vvsmal@avito.ru>
2019-03-13 13:28:59 +0300

    MA-1908: Remove unnecessary space

--- Sources/CacheStorage/Models/CacheValue.swift
+++ Sources/CacheStorage/Models/CacheValue.swift
@@ -6,7 +6,7 @@ public struct CacheValue<ChecksumType: Checksum> {
     public let entry: CacheEntry<ChecksumType>
     public let path: String
     
-    public init(entry:  CacheEntry<ChecksumType>, path: String) {
+    public init(entry: CacheEntry<ChecksumType>, path: String) {
         self.entry = entry
         self.path = path
     }
