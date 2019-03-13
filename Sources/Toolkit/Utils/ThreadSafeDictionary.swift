138af98b427e66667363c23b37617188cd822ac9
Vadim Smal <vvsmal@avito.ru>
2019-03-13 14:45:48 +0300

    MA-1908: Remove weak in ThreadSafeDictionary

--- Sources/Toolkit/Utils/ThreadSafeDictionary.swift
+++ Sources/Toolkit/Utils/ThreadSafeDictionary.swift
@@ -7,15 +7,15 @@ public final class ThreadSafeDictionary<Key: Hashable, Value> {
     private var dictionary = [Key: Value]()
     
     public func read(_ key: Key) -> (Value?) {
-        return lock.withLock { [weak self] in
-            self?.dictionary[key]
+        return lock.withLock {
+            dictionary[key]
         }
     }
     
     public func write(_ value: Value, for key: Key) {
         // In this situation, it is more correct to use the DispatchQueue, not the lock, but DispatchQueue works twice as long.
-        lock.withLock { [weak self] in
-            self?.dictionary[key] = value
+        lock.withLock {
+            dictionary[key] = value
         }
     }
     
