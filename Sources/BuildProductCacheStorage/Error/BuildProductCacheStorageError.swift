a7e3042e3cf9c62117f03dcf3d0ac88f037b8040
Vadim Smal <vvsmal@avito.ru>
2019-04-19 16:43:26 +0300

    MA-1960: Remove unnecessary errors

--- Sources/BuildProductCacheStorage/Error/BuildProductCacheStorageError.swift
+++ Sources/BuildProductCacheStorage/Error/BuildProductCacheStorageError.swift
@@ -1,19 +1,10 @@
 import Foundation
 
 public enum BuildProductCacheStorageError: Error, CustomStringConvertible {
-    case networkError(error: Error?)
-    case unableToDownloadCache(key: String)
     case unableToFindBuildProduct(path: String)
     
     public var description: String {
         switch self {
-        case let .networkError(error):
-            if let error = error {
-                return "Network error \(error)"
-            }
-            return "Network error"
-        case let .unableToDownloadCache(key):
-            return "Unable to download cached item for \(key)"
         case .unableToFindBuildProduct:
             return "Unable to find build product"
         }
