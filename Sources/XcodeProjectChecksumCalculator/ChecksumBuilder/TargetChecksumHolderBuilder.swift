8f0db49336849ee81704a4244ffe5cda589870b9
Vadim Smal <vvsmal@avito.ru>
2019-06-21 20:18:25 +0300

    MA-2101: Change swift package version

--- Sources/XcodeProjectChecksumCalculator/ChecksumBuilder/TargetChecksumHolderBuilder.swift
+++ Sources/XcodeProjectChecksumCalculator/ChecksumBuilder/TargetChecksumHolderBuilder.swift
@@ -97,14 +97,14 @@ final class TargetChecksumHolderBuilder<Builder: URLChecksumProducer> {
 extension PBXTarget {
     func fileElements() -> [PBXFileElement] {
         var files = [PBXFileElement]()
-        if let sourcesBuildPhase = try? sourcesBuildPhase(),
-            let sourcesFileElement = sourcesBuildPhase?.fileElements() {
+        if let sourcesBuildPhase = try? sourcesBuildPhase() {
+            let sourcesFileElement = sourcesBuildPhase.fileElements()
             files.append(contentsOf: sourcesFileElement)
         }
         
         if let productType = productType, case .bundle = productType {
-            if let resourcesBuildPhase = try? resourcesBuildPhase(),
-                let resourcesFileElement = resourcesBuildPhase?.fileElements() {
+            if let resourcesBuildPhase = try? resourcesBuildPhase() {
+                let resourcesFileElement = resourcesBuildPhase.fileElements()
                 files.append(contentsOf: resourcesFileElement)
             }
         }
