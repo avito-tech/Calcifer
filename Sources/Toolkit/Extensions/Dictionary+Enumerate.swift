ca7130efd8f7a525f2b63c0ada693a55fd5cd3a4
Vadim Smal <vvsmal@avito.ru>
2019-07-10 14:14:55 +0300

    MBS-5810: Throw error on fail cast

--- Sources/Toolkit/Extensions/Dictionary+Enumerate.swift
+++ Sources/Toolkit/Extensions/Dictionary+Enumerate.swift
@@ -1,20 +1,34 @@
 import Foundation
 
 public extension Dictionary {
+    
+    enum DictionaryError: Error, CustomStringConvertible {
+        case failedToCastObject
+        
+        public var description: String {
+            switch self {
+            case  .failedToCastObject:
+                return "Failed to cast object"
+            }
+        }
+    }
+    
     func enumerateKeysAndObjects(
-        options opts: NSEnumerationOptions = [],
-        using closure: (Key, Value, inout Bool) throws -> Void
+        options: NSEnumerationOptions = [],
+        iterator: (Key, Value, inout Bool) throws -> Void
     ) throws {
         var blockError: Error?
         // For performance it is very important to create a separate dictionary instance.
         // (self as NSDictionary).enumerateKeys... - works much slower
         let dictionary = NSDictionary(dictionary: self)
-        dictionary.enumerateKeysAndObjects(options: opts) { key, object, stops in
+        dictionary.enumerateKeysAndObjects(options: options) { key, object, stops in
             do {
                 var localStops = false
                 if let castedObject = object as? Value, let castedKey = key as? Key {
-                    try closure(castedKey, castedObject, &localStops)
+                    try iterator(castedKey, castedObject, &localStops)
                     stops.pointee = ObjCBool(localStops)
+                } else {
+                    throw DictionaryError.failedToCastObject
                 }
             } catch {
                 blockError = error
