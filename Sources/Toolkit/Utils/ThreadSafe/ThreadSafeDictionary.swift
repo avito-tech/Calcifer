82ebbbc304c9ddca488b4816787f21bacb35fdbe
Vadim Smal <vvsmal@avito.ru>
2019-07-10 12:34:24 +0300

    MBS-5810: Rename withLock method to whileLocked

--- Sources/Toolkit/Utils/ThreadSafe/ThreadSafeDictionary.swift
+++ Sources/Toolkit/Utils/ThreadSafe/ThreadSafeDictionary.swift
@@ -6,13 +6,13 @@ public final class ThreadSafeDictionary<Key: Hashable, Value> {
     private var dictionary = [Key: Value]()
     
     public func read(_ key: Key) -> Value? {
-        return lock.withLock {
+        return lock.whileLocked {
             dictionary[key]
         }
     }
     
     public func createIfNotExist(_ key: Key, create: (Key) throws -> (Value)) rethrows -> Value {
-        return try lock.withLock {
+        return try lock.whileLocked {
             guard let value = dictionary[key] else {
                 let value = try create(key)
                 dictionary[key] = value
@@ -24,25 +24,17 @@ public final class ThreadSafeDictionary<Key: Hashable, Value> {
     
     public func write(_ value: Value, for key: Key) {
         // In this situation, it is more correct to use the DispatchQueue, not the lock, but DispatchQueue works twice as long.
-        lock.withLock {
+        lock.whileLocked {
             dictionary[key] = value
         }
     }
     
     public var values: [Value] {
-        return lock.withLock {
+        return lock.whileLocked {
             Array(dictionary.values)
         }
     }
     
     public init() {}
     
-}
-
-extension NSLock {
-    public func withLock<T> (_ body: () throws -> T) rethrows -> T {
-        lock()
-        defer { unlock() }
-        return try body()
-    }
 }
