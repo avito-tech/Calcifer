652e474359a1d897387176ab61edfe7925c43438
Vadim Smal <vvsmal@avito.ru>
2019-07-10 14:10:19 +0300

    MBS-5810: Rename provideKey to keyProvider

--- Sources/Toolkit/Extensions/Array+KeyValue.swift
+++ Sources/Toolkit/Extensions/Array+KeyValue.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension Array {
-    func toDictionary<Key: Hashable>(_ provideKey: (Element) -> (Key)) -> [Key: Element] {
-        return Dictionary(uniqueKeysWithValues: map { (provideKey($0), $0) })
+    func toDictionary<Key: Hashable>(_ keyProvider: (Element) -> (Key)) -> [Key: Element] {
+        return Dictionary(uniqueKeysWithValues: map { (keyProvider($0), $0) })
     }
 }
